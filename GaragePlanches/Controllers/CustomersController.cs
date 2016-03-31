6b73e403561ef7f2fda096126b9b3d5ed73efa86
Bruno <bsalemi@mobatime.ch>
2016-03-31 15:23:06 +0200

    update customersController + view

--- GaragePlanches/Controllers/CustomersController.cs
+++ GaragePlanches/Controllers/CustomersController.cs
@@ -47,7 +47,7 @@ namespace GaragePlanches.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "CustomerID,Firstname,Lastname")] Customer customer)
+        public async Task<ActionResult> Create([Bind(Include = "CustomerID,Firstname,Lastname,Address")] Customer customer)
         {
             if (ModelState.IsValid)
             {
@@ -79,7 +79,7 @@ namespace GaragePlanches.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "CustomerID,Firstname,Lastname")] Customer customer)
+        public async Task<ActionResult> Edit([Bind(Include = "CustomerID,Firstname,Lastname,Address")] Customer customer)
         {
             if (ModelState.IsValid)
             {
